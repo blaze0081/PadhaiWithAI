c41fc2c0de36eccb616159b6983f0da998c9417f
blaze0081 <bhaskarmishra008@gmail.com>
2025-01-02 18:54:33 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-cd school_project
-gunicorn school_project.wsgi:application --log-file - --bind 0.0.0.0:8001
+gunicorn school_project.school_project.wsgi:application --log-file -
